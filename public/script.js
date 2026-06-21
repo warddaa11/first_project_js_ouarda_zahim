661c9314eeafc1e15421854576b291353458d8e8
warddaa11 <warda1169@hotmail.com>
2026-06-21 14:55:42 +0100

    Add users database array

---
+++ public/script.js
@@ -0,0 +1,21 @@
+let user = [
+    {
+        name: "ouarda ouarda",
+        email: "ouardaouarda@gmail.com",
+        password: "12345678@",
+        balance: 1000,
+        loan : 0,
+        investement:[],
+        history: []
+}
+];
+
+// let user = {
+//         name: "ouarda ouarda",
+//         email: "ouardaouarda@gmail.com",
+//         password: "1234",
+//         balance: 1000,
+//         loan : 0,
+//         investement:[],
+//         history: []
+// };
