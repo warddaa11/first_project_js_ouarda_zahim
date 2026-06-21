f725fc27986fe212c0d3fa0cebd094abb6870e9c
warddaa11 <warda1169@hotmail.com>
2026-06-21 14:57:33 +0100

    Create main menu with prompts

--- public/script.js
+++ public/script.js
@@ -19,3 +19,29 @@ let user = [
 //         investement:[],
 //         history: []
 // };
+
+
+while (true) {
+    let choice=prompt("1- Sign Up \n 2- Login \n 3- Change Password")
+    
+    
+    if (choice === "1") {
+        signUp();
+       
+
+    }else if (choice === "2") {
+        let loginUser = login();
+        if (loginUser) {
+            bankSysytem(loginUser) 
+        }
+        
+    }else if (choice === "3") {
+       changePassword()
+
+    }
+    if (choice === "exit") {
+        break;
+        
+    }
+  
+}
